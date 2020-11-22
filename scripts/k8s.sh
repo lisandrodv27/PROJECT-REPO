0836ae2b97506d9fa6ea28d1544bead3e14df47b
lisandrodv27 <71394551+lisandrodv27@users.noreply.github.com>
2020-11-22 17:14:22 +0000

    Update k8s.sh

--- scripts/k8s.sh
+++ scripts/k8s.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cd K8s-project
-kubectl apply -f secret.yaml -n dev-ops-project
-kubectl apply -f lb.yaml -f nginx-cm.yaml -f nginx.yaml -n dev-ops-project
-kubectl apply -f cl-ip-front.yaml -f frontend.yaml -n dev-ops-project
-kubectl apply -f cl-ip-back.yaml -f backend.yaml -n dev-ops-project
-kubectl get svc -n dev-ops-project
+sudo kubectl apply -f secret.yaml -n dev-ops-project
+sudo kubectl apply -f lb.yaml -f nginx-cm.yaml -f nginx.yaml -n dev-ops-project
+sudo kubectl apply -f cl-ip-front.yaml -f frontend.yaml -n dev-ops-project
+sudo kubectl apply -f cl-ip-back.yaml -f backend.yaml -n dev-ops-project
+sudo kubectl get svc -n dev-ops-project
