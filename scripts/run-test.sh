a98e9ff70bbeecc1590a05d8044aeb1c7d484f63
lisandrodv27 <71394551+lisandrodv27@users.noreply.github.com>
2020-11-22 00:09:54 +0000

    Update run-test.sh

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo docker-compose up -d
+sudo -E DATABASE_URI=${DATABASE_URI} -E TEST_DATABASE_URI=${TEST_DATABASE_URI} -E SECRET_KEY=${SECRET_KEY} docker-compose up -d
 sudo docker exec backend bash -c 'pytest tests/ --cov application'
 sudo docker exec frontend bash -c 'pytest tests/ --cov application'
 sudo docker-compose down
