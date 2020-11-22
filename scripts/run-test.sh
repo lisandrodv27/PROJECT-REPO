f55168ee24f5e1a19d534397b47283de8fe91c24
lisandrodv27 <71394551+lisandrodv27@users.noreply.github.com>
2020-11-22 17:07:10 +0000

    Update run-test.sh

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -4,3 +4,4 @@ sudo -E DATABASE_URI=${DATABASE_URI} -E TEST_DATABASE_URI=${TEST_DATABASE_URI} -
 sudo docker exec backend bash -c 'pytest tests/ --cov application'
 sudo docker exec frontend bash -c 'pytest tests/ --cov application'
 sudo docker-compose down
+rm -rf PROJECT-REPO
