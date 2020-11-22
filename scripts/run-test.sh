d6edf9cb2e12156145665a172241bfca31b081ab
lisandrodv27 <71394551+lisandrodv27@users.noreply.github.com>
2020-11-22 21:18:21 +0000

    Update run-test.sh

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo -E DATABASE_URI=${DATABASE_URI} -E TEST_DATABASE_URI=${TEST_DATABASE_URI} -E SECRET_KEY=${SECRET_KEY} docker-compose up -d
+sudo -E DATABASE_URI=${DATABASE_URI} -E TEST_DATABASE_URI=${TEST_DATABASE_URI} -E SECRET_KEY=${SECRET_KEY} docker-compose up -d --build
 mysql -h ${TEST_DATABASE_ENDP} -P 3306 -u ${TEST_DATABASE_USER} -p${TEST_DB_PASSWD} < database/Create.sql
 sudo docker exec backend bash -c "pytest tests/ --cov application"
 sudo docker exec frontend bash -c "pytest tests/ --cov application"
