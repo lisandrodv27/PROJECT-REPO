a657c1670dbaccbcb9f3ffba7a9f6557e0741f06
lisandrodv27 <71394551+lisandrodv27@users.noreply.github.com>
2020-11-23 01:48:15 +0000

    Update build-push.sh

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -3,7 +3,11 @@
 cd frontend
 sudo docker build -t lisandrodv27/frontend-build:latest .
 sudo docker push lisandrodv27/frontend-build:latest
+sudo docker rmi lisandrodv27/frontend-build
 cd ..
 cd backend
 sudo docker build -t lisandrodv27/backend-build:latest .
 sudo docker push lisandrodv27/backend-build:latest
+sudo docker rmi lisandrodv27/backend-build
+
+
